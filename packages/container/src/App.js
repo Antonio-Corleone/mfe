8b5cbe0c2834c036223d7004538aad9c840af689
TuanVM5 <tuanvm5@fpt.com>
2025-02-12 08:26:41 +0700

    first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingApp from './containers/MarketingApp';
+
+export default function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <MarketingApp />
+    </div>
+  );
+}
