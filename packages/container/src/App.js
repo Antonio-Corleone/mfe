29db16cf39383c26741299e337fe25c788532ad9
TuanVM5 <tuanvm5@fpt.com>
2025-02-12 09:30:36 +0700

    ci: fix scripts

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,6 @@ import MarketingApp from './containers/MarketingApp';
 export default function App() {
   return (
     <div>
-      {/* Test run scripts */}
       <h1>My App</h1>
       <MarketingApp />
     </div>
