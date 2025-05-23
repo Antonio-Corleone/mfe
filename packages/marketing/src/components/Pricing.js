54f740cdf6a68c8be84edce58ae02b84dcbb592f
AntonioVu <vuminhtuan3795@gmail.com>
2025-05-23 08:57:24 +0700

    update check github action

--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -153,7 +153,7 @@ export default function Pricing() {
           color="textPrimary"
           gutterBottom
         >
-          Pricing
+          Pricing updated
         </Typography>
         <Typography
           variant="h5"
